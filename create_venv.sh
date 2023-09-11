53f5f10368622109127f71ca06643ad37768a14b
Angelica Iacovelli <angelicaiacovelli99@libero.it>
2023-09-11 15:24:57 -0700

    Training set up

--- create_venv.sh
+++ create_venv.sh
@@ -12,3 +12,18 @@ pip install packaging
 pip install dgl
 pip install torch
 pip install vtk
+git install hydra
+
+git clone https://github.com/NVIDIA/modulus.git
+cd modulus 
+pip install .
+cd ..
+rm -rf modulus
+
+git clone https://github.com/NVIDIA/modulus-launch.git
+cd modulus-launch 
+pip install .
+cd ..
+rm -rf modulus-launch
+
+pip install pydantic==1.10.9
